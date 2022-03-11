60fa644b2b63a58e86dfec00900a59b6b769753d
Tshepo Tsilo <tshepo9099@gmail.com>
2022-03-11 14:15:07 +0200

    implements image upload functionality

---
+++ src/components/products/product-setup.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Box from "../box";
+import ProductForm from "../../forms/store/product";
+
+const ProductSetup = () => {
+  return (
+    <Box className="text-center">
+      <ProductForm />
+    </Box>
+  );
+};
+
+export default ProductSetup;
